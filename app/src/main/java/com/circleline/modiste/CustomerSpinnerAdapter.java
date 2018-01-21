5891879af75174b562ab1a557dd77912920c9f56
Rahadyan Fannani Arif <fanni@klopos.com>
2018-01-21 21:49:35 +0700

    fixing pilih customer

--- app/src/main/java/com/circleline/modiste/CustomerSpinnerAdapter.java
+++ app/src/main/java/com/circleline/modiste/CustomerSpinnerAdapter.java
@@ -23,8 +23,20 @@ public class CustomerSpinnerAdapter extends ArrayAdapter<Customer> {
     private Context context;
     public CustomerSpinnerAdapter(@NonNull Context context, int resource, List<Customer> customerList) {
         super(context, resource);
+
         this.customerList = customerList;
         this.context = context;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        if (position == 0) {
+            // Disable the first item from Spinner
+            // First item will be use for hint
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -51,8 +63,16 @@ public class CustomerSpinnerAdapter extends ArrayAdapter<Customer> {
     @Override
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         TextView label = new TextView(context);
-        label.setTextColor(Color.BLACK);
+        if (position == 0) {
+            // Set the hint text color gray
+            label.setTextColor(Color.GRAY);
+        } else {
+            label.setTextColor(Color.BLACK);
+        }
         label.setText(customerList.get(position).getNama());
         return label;
     }
+
+
+
 }
--- app/src/main/java/com/circleline/modiste/OrderFormActivity.java
+++ app/src/main/java/com/circleline/modiste/OrderFormActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.circleline.modiste.models.Customer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -22,20 +23,37 @@ public class OrderFormActivity extends AppCompatActivity {
 
     @BindView(R.id.spnr_pemesan) Spinner spnr_pemesan;
 
+    List<Customer> customerList = new ArrayList<Customer>();
+    private CustomerSpinnerAdapter customerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_form);
         ButterKnife.bind(this);
 
-        List<Customer> customerList = Customer.listAll(Customer.class);
-        final CustomerSpinnerAdapter customerAdapter = new CustomerSpinnerAdapter(OrderFormActivity.this,android.R.layout.simple_spinner_item,customerList);
+        List<Customer> list = Customer.listAll(Customer.class);
+        generateCustomerList(list);
+
+        customerAdapter = new CustomerSpinnerAdapter(OrderFormActivity.this,android.R.layout.simple_spinner_item,customerList);
         spnr_pemesan.setAdapter(customerAdapter);
         spnr_pemesan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Customer customer = customerAdapter.getItem(i);
-                Toast.makeText(OrderFormActivity.this,customer.getId().toString(),Toast.LENGTH_LONG).show();
+
+               // String selectedItemText = (String) adapterView.getItemAtPosition(i);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if (i > 0) {
+                    // Notify the selected item text
+                   // Toast.makeText(getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
+                    Customer customer = customerAdapter.getItem(i);
+                    Toast.makeText(OrderFormActivity.this,customer.getNama().toString(),Toast.LENGTH_LONG).show();
+
+
+
+                }
+
 
             }
             @Override
@@ -54,8 +72,18 @@ public class OrderFormActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == Constant.CREATE_CUSTOMER_REQUEST ) {
             if (resultCode == RESULT_OK) {
-
+                List<Customer> list = Customer.listAll(Customer.class);
+                generateCustomerList(list);
+                customerAdapter.notifyDataSetChanged();
+                spnr_pemesan.setSelection(list.size()+1);
             }
         }
+    }
+
+    private void generateCustomerList(List<Customer> list){
+        customerList.clear();
+        Customer hint = new Customer("Pilih Pemesan","-1","-1");
+        customerList.add(hint);
+        customerList.addAll(list);
     }
 }
